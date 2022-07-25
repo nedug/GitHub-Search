bbd51fd7f31123c705bdfe7c5a8d613e152c57d0
Alexander R <ru5nedug@mail.ru>
2022-07-25 14:06:30 +0300

    input

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
+
     return (
         <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
             <h3 className="font-bold">GitHub Search</h3>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchUsersQuery } from '../store/github/github.api';
 
 export const HomePage = () => {
+    const [search, setSearch] = useState('');
 
-    const {isLoading, isError, data} = useSearchUsersQuery('nedug');
+    const { isLoading, isError, data } = useSearchUsersQuery('nedug');
 
-    console.log(isLoading, isError, data)
+    // console.log(isLoading, isError, data)
+
+
+
+
+    useEffect(() => {
+        console.log(search)
+    }, [search])
+
 
     return (
-        <div>
-            Home
+        <div className="flex-col justify-center pt-10 mx-auto h-screen w-screen">
+
+            {isError && <p className="text-center text-red-600">Something went wrong...</p>}
+
+            <div className="relative w-[560px]">
+                <input
+                    type="text"
+                    className="border px-4 py-2 w-full h-[42px] mb-2"
+                    placeholder="Search for GitGub username..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+
+                <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, voluptatum.
+                </div>
+            </div>
+
+
+            {/*{*/}
+            {/*    data?.map(({ login, id }) => {*/}
+            {/*        return <div key={id}>{login}</div>*/}
+            {/*    })*/}
+            {/*}*/}
         </div>
     );
 };
