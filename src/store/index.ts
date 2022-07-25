5998b81881424a67665c91c0d65cc67fa91110f6
Alexander R <ru5nedug@mail.ru>
2022-07-25 13:34:19 +0300

    middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,4 +6,5 @@ export const store = configureStore({
     reducer: {
         [githubApi.reducerPath]: githubApi.reducer,
     },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(githubApi.middleware),
 });
