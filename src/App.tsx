ad4788aaad60fa3337f9a8c172e3caf3137bce4a
Alexander R <ru5nedug@mail.ru>
2022-07-25 12:09:03 +0300

    congig

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  );
+    return (
+        <h1 className="font-bold">Hello React</h1>
+    );
 }
 
 export default App;
